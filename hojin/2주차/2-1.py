1b13c0a51e02dbf525edef71b7578f56b60b472e
hovelopin <kimhojin3714@naver.com>
2022-01-20 13:56:40 +0900

    BOJ 2583 , PG 멀쩡한 사각형 , PG 괄호회전

---
+++ hojin/2주차/2-1.py
@@ -0,0 +1,21 @@
+# 구현을 못했습니다.
+'''
+8,6의 사각형을 예를 들어 기울기는 3/4이다. x값에 1,2,3,4를 대입했을때 격자점 4,3에서 만난다.
+그동안 잘린 사각형의 갯수를 세어보면 6개이다.
+파이썬에서는 최대 공약수의 개수를 구하는 것을 math 모듈내에 gcd 내장함수를 이용하면 바로 찾을수있다.
+'''
+
+def func(w,h):
+    a,b = max([w,h]),min([w,h])
+    while(True):
+        r=a%b
+        if r==0:
+            return b
+        a = b
+        b = r
+
+def solution(w,h):
+    squares = w*h
+    gcd = func(w,h)
+    return squares - (w+h-gcd)
+
---
+++ hojin/2주차/2-3.py
@@ -0,0 +1,49 @@
+# 큐를 사용하기 위한 모듈
+from collections import deque
+
+M, N, K = map(int, input().split())
+
+# 위 , 아래 , 왼쪽 , 오른쪽
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+# 0으로 채워진 2차원 배열을 만든다.
+graph = [list([0] * N) for _ in range(M)]
+
+# 지정된 영역을 색칠한다.
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] = -1
+
+
+resultList = []
+
+# bfs 탐색 방식을 이용해서 해결한다!
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 0:
+            count = 1
+            queue = deque([])
+            queue.append([i, j])
+            graph[i][j] = count
+            while queue:
+                y, x = queue.popleft()
+                # 상하좌우 방향을 움직여가며 확인한다
+                for k in range(4):
+                    nY = y + dy[k]
+                    nX = x + dx[k]
+                    # 지난번 달팽이 배열 문제랑 똑같이 작동 
+                    if 0 <= nX < N and 0 <= nY < M and graph[nY][nX] == 0:
+                        graph[nY][nX] = 1
+                        queue.append([nY, nX])
+                        count += 1
+
+            resultList.append(count)
+
+print(len(resultList))
+resultList.sort()
+
+for num in resultList:
+    print(num,end=" ")
